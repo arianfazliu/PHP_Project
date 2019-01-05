fca18cbd29aa4c440ea612b342d9d9b801c734ef
boredalis <aurora.selimi@studentet.uni-pr.edu>
2019-01-05 22:15:14 +0100

    small changes

--- js/canvas.js
+++ js/canvas.js
@@ -1,22 +1,25 @@
-//canvas
-    // set up initial variables
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
- 
-  ctx.beginPath();
-  ctx.moveTo(100,0);
-  ctx.lineTo(100,200);
-  ctx.stroke();
+	// set up initial variables
 
-function drawCircle(x){
-  ctx.beginPath();
-  ctx.arc(x,100,10,0,2*Math.PI);
-  ctx.fillStyle="red";
-  ctx.fill();
-}
-var x = 0;
-setInterval(function(){ 
-  ctx.clearRect(0,0,200,200);
-  drawCircle(x%200);
-  x++;
-}, 25);
+window.addEventListener('load', function () {
+	console.log("Not qowrking");
+    var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+ 
+	  ctx.beginPath();
+	  ctx.moveTo(100,0);
+	  ctx.lineTo(100,200);
+	  ctx.stroke();
+
+	function drawCircle(x){
+	  ctx.beginPath();
+	  ctx.arc(x,100,10,0,2*Math.PI);
+	  ctx.fillStyle="red";
+	  ctx.fill();
+	}
+	var x = 0;
+	setInterval(function(){ 
+	  ctx.clearRect(0,0,200,200);
+	  drawCircle(x%200);
+	  x++;
+	}, 25);
+});
