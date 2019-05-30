69ff7d8c4b44e3e571b4578b9f67f04dbdc5f767
boredalis <aurora.selimi@studentet.uni-pr.edu>
2019-05-30 15:05:43 +0200

    small fixes

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2,16 +2,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <meta charset="UTF-8">
-    <title>Quiz</title>
-=======
     <title>Dashboard</title>
-    
->>>>>>> 61160f28ec40580f2c0fbb8d0e48b4eec107038a
-</head>
+    </head>
 <body>
-<<<<<<< HEAD
   <div class="homepage">
     <div class="displayContent">
         <img src="../img/search.png" style="height: 120%;width: 80%;position: absolute;top:-100px;left:auto;right:auto;z-index:0;" alt="">
@@ -25,88 +18,66 @@
         </div>
     </div>
   </div>
-=======
-
-<div class="homepage">
-   <?php include('../components/header.php') ?>
-	<div class="displayContent">
-      <img src="../img/search.png" style="height: 120%;width: 80%;position: absolute;top:-100px;left:auto;right:auto;z-index:0;" alt="">
-      <div style="z-index: 1;">
-        <form autocomplete="off" class="example" action="javascript:void(0)">
-          <input type="text" placeholder="Search" name="search" onkeyup="showsuggestion(this.value);">
-        </form>
-      </div>
-      <div id="divsuggestion" style="z-index: 10;">
-      </div>
-	</div>
-   <?php include('../components/footer.php') ?>
-</div>
->>>>>>> 61160f28ec40580f2c0fbb8d0e48b4eec107038a
 </body>
 <style type="text/css">
-body,html{
-  height:calc(100% - 100px);
-  box-sizing: border-box;
-  background-color: #fafafa;
-}
-.homepage{
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-.displayContent{
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: auto;
-  background-color: white;
-  justify-content: center;
-  align-items: center;
-  overflow-y:hidden;
-  height: 100%;
-}
-* {
-  box-sizing: border-box;
-}
+  body,html{
+    height:calc(100% - 100px);
+    box-sizing: border-box;
+    background-color: #fafafa;
+  }
+  .homepage{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .displayContent{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: auto;
+    background-color: white;
+    justify-content: center;
+    align-items: center;
+    overflow-y:hidden;
+    height: 100%;
+  }
+  * {
+    box-sizing: border-box;
+  }
 
-form.example input[type=text] {
-  padding: 10px;
-  font-size: 17px;
-  width:500px ;
-  border-radius: 2px;
-  border:none;
-  outline: none;
-  background: #f1f1f1;
-}
-form.example::after {
-  content: "";
-  clear: both;
-  display: table;
-}
+  form.example input[type=text] {
+    padding: 10px;
+    font-size: 17px;
+    width:500px ;
+    border-radius: 2px;
+    border:none;
+    outline: none;
+    background: #f1f1f1;
+  }
+  form.example::after {
+    content: "";
+    clear: both;
+    display: table;
+  }
 </style>
 
 <script>
-function showsuggestion(str) {
-    if (str.length == 0) { 
-        document.getElementById("divsuggestion").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("divsuggestion").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "getsuggestion.php?q=" + str, true);
-        xmlhttp.send();
-    }
-}
-<<<<<<< HEAD
+  function showsuggestion(str) {
+      if (str.length == 0) { 
+          document.getElementById("divsuggestion").innerHTML = "";
+          return;
+      } else {
+          var xmlhttp = new XMLHttpRequest();
+          xmlhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                  document.getElementById("divsuggestion").innerHTML = this.responseText;
+              }
+          };
+          xmlhttp.open("GET", "getsuggestion.php?q=" + str, true);
+          xmlhttp.send();
+      }
+  }
 </script>
 </html>
 <?php require('../components/footer.php') ?>
-=======
-
-</script>
->>>>>>> 61160f28ec40580f2c0fbb8d0e48b4eec107038a
