37d50616b4d9809f24084abf32d34a2a6d7c8eec
Arita <arita.berisha@studentet.uni-pr.edu>
2019-05-30 18:09:36 +0200

    header

--- components/header.php
+++ components/header.php
@@ -69,20 +69,20 @@
 			  } 
 			}
 			?>
-			<!-- pathi arian 
+			<!-- pathi arian -->
 			<li><a class="spanat <?php active('dashboard.php');?>" href="http://localhost:82/PHP_project/dashboard/dashboard.php">Home</a></li>
 		    <li><a class="spanat <?php active('languages.php');?>" href="http://localhost:82/PHP_project/dashboard/languages.php">Languages</a></li>
 		    <li><a class="spanat <?php active('quiz.php');?>" href="http://localhost:82/PHP_project/dashboard/quiz.php">Quiz</a></li>
-		    <li><a class="spanat <?php active('contactus.php');?>" href="http://localhost:82/PHP_project/dashboard/contactus.php">Contact Us</a></li>-->
+		    <li><a class="spanat <?php active('contactus.php');?>" href="http://localhost:82/PHP_project/dashboard/contactus.php">Contact Us</a></li>
 
 
 		    <!-- 
-			pathi aurora-->
+			pathi aurora
 		    <li><a class="spanat <?php active('dashboard.php');?>" href="http://localhost/Webucator/ClassFiles/PHPBasics/Demos/PHP_project/dashboard/dashboard.php">Home</a></li>
 		    <li><a class="spanat <?php active('languages.php');?>" href="http://localhost/Webucator/ClassFiles/PHPBasics/Demos/PHP_project/dashboard/languages.php">Languages</a></li>
 		    <li><a class="spanat <?php active('quiz.php');?>" href="http://localhost/Webucator/ClassFiles/PHPBasics/Demos/PHP_project/dashboard/quiz.php">Quiz</a></li>
 		    <li><a class="spanat <?php active('contactus.php');?>" href="http://localhost/Webucator/ClassFiles/PHPBasics/Demos/PHP_project/dashboard/contactus.php">Contact Us</a></li> 
-			<button onclick="window.location='main.php'" class="btn">LOG OUT</button>
+			<button onclick="window.location='main.php'" class="btn">LOG OUT</button>-->
 		</div>
 	</div>
 </body>
