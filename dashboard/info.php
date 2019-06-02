a3ebb7d4234ff8f6d106cc84d2b2025e182cf89b
Arita <arita.berisha@studentet.uni-pr.edu>
2019-06-02 22:14:33 +0200

    nje klase tjeter

--- dashboard/info.php
+++ dashboard/info.php
@@ -1,5 +1,15 @@
 <?php
 include('../components/header.php');
+class Info{
+  var $information;
+  var $color;
+
+  public function __construct($information,$color)
+  {
+    $this->information=$information;
+    $this->color=$color;
+  }
+}
 ?>
 <head>
     <title>Info</title>
@@ -8,45 +18,27 @@ include('../components/header.php');
 <div class="homepage">
 <div class="background">
 <h1 style="text-align: center; color: #888D8A; font-family: Pretty"> About us </h1>
-<p style="color: #888D8A; font-size:20px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Vivamus sagittis metus libero, sit amet hendrerit urna egestas id.
-     Fusce pulvinar tempus efficitur. Sed vel sodales lectus. 
-     In hac habitasse platea dictumst. Morbi eget felis nec mi varius blandit.
-      Quisque non risus et quam aliquam iaculis eget et est.
-       Donec nisl eros, sagittis id viverra sed, suscipit ac dui.
-        Sed accumsan vulputate eros eget vestibulum.
-         Aenean scelerisque orci in velit sagittis pretium.
-          Duis malesuada faucibus justo, et vestibulum ex finibus nec.
-           Nunc lacinia volutpat vestibulum.
-            Curabitur sollicitudin, risus et semper ullamcorper, metus lectus placerat tellus,
-             et vestibulum dui odio nec leo.<br><br>
+<?php  
+$string="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Vivamus sagittis metus libero, sit amet hendrerit urna egestas id.
+ Fusce pulvinar tempus efficitur. Sed vel sodales lectus. 
+ In hac habitasse platea dictumst. Morbi eget felis nec mi varius blandit.
+  Quisque non risus et quam aliquam iaculis eget et est.
+   Donec nisl eros, sagittis id viverra sed, suscipit ac dui.
+    Sed accumsan vulputate eros eget vestibulum.
+     Aenean scelerisque orci in velit sagittis pretium.
+      Duis malesuada faucibus justo, et vestibulum ex finibus nec.
+       Nunc lacinia volutpat vestibulum.
+        Curabitur sollicitudin, risus et semper ullamcorper, metus lectus placerat tellus,
+         et vestibulum dui odio nec leo.";
+$info =  new Info($string, "#888D8A");
+$info2= new Info($string,"#BDC2BF");
+$info3 = new Info($string,"#D2DED7");
+         echo "<p style=\"color:$info->color\">".$info->information;
+         echo "<p style=\"color:$info2->color\">".$info2->information;
+         echo "<p style=\"color:$info3->color\">".$info3->information;
 
-Sed ullamcorper mollis venenatis. 
-Aliquam eget ex rutrum, viverra ligula eget, semper justo. Duis et tellus dui.
- Pellentesque molestie elit vitae augue suscipit lacinia. In vestibulum in diam non scelerisque.
-  Suspendisse ipsum diam, mattis sed ullamcorper eget, consequat non neque. 
-  Donec suscipit sapien nec elit mollis, vitae tempus mi rhoncus. 
-  Nullam vulputate consectetur sagittis. 
-  Nulla est felis, finibus sed nibh id, porttitor viverra arcu.
-   Donec porttitor, risus varius laoreet tristique, erat augue dictum magna, quis dignissim ipsum massa ut sapien.
-    Pellentesque lobortis, arcu eget iaculis mattis, lorem nisl euismod ipsum, nec pulvinar leo risus ut nisl. 
-    Phasellus laoreet ullamcorper nisi. 
-    Donec mollis condimentum leo, eu gravida massa varius in. Nulla facilisi. 
-    Donec posuere pretium ipsum.<br><br>
-
-Donec vulputate lorem ac eros accumsan volutpat.
- Vestibulum pharetra diam in tempus consequat. 
- Phasellus in volutpat eros. 
- In sollicitudin in odio ac venenatis. 
- Maecenas commodo lacus at viverra pharetra. 
- Duis vestibulum tortor non justo maximus cursus. 
- Nulla pulvinar massa ac lobortis hendrerit. 
- Praesent tortor est, cursus lacinia mollis vel, condimentum at mi. 
- Proin nisi mauris, vehicula eu justo id, venenatis dictum nulla. 
- Sed vehicula vulputate mauris id scelerisque. 
- Sed dapibus ante sed sapien placerat cursus.
-  Praesent nec ligula et tortor egestas viverra. 
-  Vestibulum accumsan ex. </p>
+?>
 </div>
 
 </div>
