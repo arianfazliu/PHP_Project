8d1b4510355dd09207d1361bf4dc6219df8c9e92
Arita <arita.berisha@studentet.uni-pr.edu>
2019-06-02 23:08:25 +0200

    end

--- dashboard/lang.php
+++ dashboard/lang.php
@@ -30,7 +30,6 @@ try
 {
     $db= new PDO("mysql:host=".DBHOST.";dbname=".DBNAME,DBUSER,DBPASS);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Lidhja me DB u realizua me sukses!";
 }
 catch(PDOException $pdoExcep)
 {
